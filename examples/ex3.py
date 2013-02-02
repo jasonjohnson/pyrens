96e7e8ae780e2328fc5c454bc4ba4b7622b193ba
Jason Johnson <spligak@gmail.com>
2013-02-02 16:57:55 -0600

    remove old files

--- examples/ex3.py
+++
@@ -1,4 +0,0 @@
-from pyrens.runtime import *
-def _fn_0_1(arg1,arg2,arg3): return _if(_gt(_mul(arg1,arg2,arg3),20),100,200)
-power = _fn_0_1
-_print(power(1,2,3))
--- examples/ex5.py
+++
@@ -1,5 +0,0 @@
-from pyrens.runtime import *
-import requests
-def _fn_1_1(url): return requests.get(url)
-fetch = _fn_1_1
-_print(fetch("http://www.google.com/"))
